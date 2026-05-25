6a22e8d31aa9542ec850fcabadda8ca0d02788d4
veryl <veryl@LILITH.localdomain>
2026-05-25 13:50:45 +0530

    chore(release): bump version to 0.4 and add APK asset

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.heracles.mobile"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "0.4"
     }
 
     buildFeatures {
